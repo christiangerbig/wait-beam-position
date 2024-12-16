ace10f5524049d4bd0bb0a2cde818789934c4d16
christiangerbig <chr_gerbig@web.de>
2024-12-16 19:12:55 +0100

    update

--- wait-beam-position.asm
+++ wait-beam-position.asm
@@ -5,49 +5,43 @@ COLOR00				EQU $180
 
 _CIAA				EQU $bfe001
 CIAPRA				EQU $000
-CIAB_GAMEPORT0			EQU 6	; Left mouse button
+CIAB_GAMEPORT0			EQU 6	; left mouse button
 
-vert_beam_position		EQU $135 ; PAL display (total short frame = $137, long frame = $138), minus 3 scanlines for swapping playfields and setting bitplane pointers
-
-rgb4_red			EQU $f00
+vert_beam_position		EQU $136 ; PAL
 
 
-
+; Input
+; Result
+	CNOP 0,4
 main
 	lea	_CIAA,a4
 	lea	_CUSTOM,a6
-
 main_loop
 	bsr	wait_vert_beam_position
-;	bsr	swap_playfields
 ;	bsr	routine1
 ;	bsr	routine2
 ;	bsr	routine3
-	move.w	#rgb4_red,COLOR00(a6)
 	btst	#CIAB_GAMEPORT0,CIAPRA(a4)
 	bne.s	main_loop
+	rts
 
 
-; Wait for a specific vertical beam position
 ; Input
-; a6	... Custom chips base
+; a6	Custom chips base
 ; Result
-; d0	... No return value
 	CNOP 0,4
 wait_vert_beam_position
-	move.l	#$0003ff00,d1		; Mask out non vertical position bits
+	move.l	#$0003ff00,d1		; only vertical position
 	move.l  #vert_beam_position<<8,d2
 	lea	VPOSR(a6),a0
 	lea	VHPOSR(a6),a1
 wait_vert_beam_position_loop
-	move.w	(a0),d0			; Get VPOSR bits
-	swap	d0			; and shift them to the upper word
-	move.w	(a1),d0			; Get VHPOSR bits lower word
-	and.l	d1,d0			; Only vertical position
-	cmp.l	d2,d0			; Wait for the rasterline
+	move.w	(a0),d0			; VPOSR
+	swap	d0			; adjust bits
+	move.w	(a1),d0			; VHPOSR
+	and.l	d1,d0			; only vertical position
+	cmp.l	d2,d0			; vertical beam position reached ?
 	blt.s	wait_vert_beam_position_loop
 	rts
-
-; ... routines
 
 	END
