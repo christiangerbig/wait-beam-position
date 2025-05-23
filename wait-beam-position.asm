c23e50dfec4b0fb8376d4aba54d45f1911934d9f
christiangerbig <chr_gerbig@web.de>
2025-05-23 20:33:28 +0200

    improved for 680x0 machines

--- wait-beam-position.asm
+++ wait-beam-position.asm
@@ -7,7 +7,9 @@ _CIAA				EQU $bfe001
 CIAPRA				EQU $000
 CIAB_GAMEPORT0			EQU 6	; left mouse button
 
-vert_beam_position		EQU $136 ; PAL
+beam_position			EQU $136 ; PAL: bottom of frame
+
+vertical_position_bits		EQU $3ff ; V0..V9 position bits
 
 
 ; Input
@@ -17,9 +19,9 @@ main
 	lea	_CIAA,a4
 	lea	_CUSTOM,a6
 main_loop
-	bsr	wait_vert_beam_position
+	bsr	wait_beam_position
 ;	bsr	routine1
-;	bsr	routine2
+;	bsr     routine2
 ;	bsr	routine3
 	btst	#CIAB_GAMEPORT0,CIAPRA(a4)
 	bne.s	main_loop
@@ -27,21 +29,28 @@ main_loop
 
 
 ; Input
-; a6	Custom chips base
+; a6.l	Custom chips base
 ; Result
 	CNOP 0,4
-wait_vert_beam_position
-	move.l	#$0003ff00,d1		; only vertical position
-	move.l  #vert_beam_position<<8,d2
+wait_beam_position
+	move.l	#vertical_position_bits<<8,d1
+	move.l	#beam_position<<8,d2
 	lea	VPOSR(a6),a0
 	lea	VHPOSR(a6),a1
-wait_vert_beam_position_loop
-	move.w	(a0),d0			; VPOSR
-	swap	d0			; adjust bits
-	move.w	(a1),d0			; VHPOSR
-	and.l	d1,d0			; only vertical position
-	cmp.l	d2,d0			; vertical beam position reached ?
-	blt.s	wait_vert_beam_position_loop
+wait_beam_position_loop1
+	move.w	(a0),d0
+	swap	d0			; high word: VPOSR
+	move.w	(a1),d0			; low word: VHPOSR
+	and.l	d1,d0			; vertical position
+	cmp.l	d2,d0			; one position per frame on 680x0 machines
+	bge.s	wait_beam_position_loop1
+wait_beam_position_loop2
+	move.w	(a0),d0
+	swap	d0			; high word: VPOSR
+	move.w	(a1),d0			; low word: VHPOSR
+	and.l	d1,d0			; vertical position
+	cmp.l	d2,d0			; beam position reached ?
+	blt.s	wait_beam_position_loop2
 	rts
 
 	END
